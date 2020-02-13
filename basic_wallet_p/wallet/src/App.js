1bfa91d072ff21d5f4e2dc3a17abd6797e005895
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-13 17:26:58 -0500

    Basic login and send/receive status Can filter transactions by user Received shows green, Sent shows red

--- basic_wallet_p/wallet/src/App.js
+++ basic_wallet_p/wallet/src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 function App() {
   let [chain,saveChain]=useState(null);
+  let [user,setUser]=useState(24601);
+
   useEffect(() => {
     axios.get("http://127.0.0.1:5000/chain")
     .then((res) => {
@@ -15,19 +17,24 @@ function App() {
     })
   },[])
 
-  // if (chain!=null){
-  //   console.log("YEY");
-  // }
 
   return (
     <div className="App">
       New Wallet App
+      <div className="login">
+        <input value={user} onChange={(e)=>setUser(e.target.value)} />
+      </div>
       <div className="transactions">
+  
         <h2>Transactions</h2>
         <ul>
           {chain!=null&& <li>Thing</li>&&chain.map(item => {
               return item.transactions.map(trans => {
-                return <li key={item.index+trans.recipient}>{`Transfer ${trans.amount} from ${trans.sender} to ${trans.recipient}`}</li>
+                if (trans.sender==user){
+                  return <li key={item.index+trans.recipient} className="Sent">{`Transfer ${trans.amount} from ${trans.sender} to ${trans.recipient}`}</li>
+                }else if (trans.recipient==user){
+                  return <li key={item.index+trans.recipient} className="Recieve">{`Transfer ${trans.amount} from ${trans.sender} to ${trans.recipient}`}</li>
+                }
               })
           })}
         </ul>
